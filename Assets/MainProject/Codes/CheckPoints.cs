a1c0b606e70048092e26aebec5f4be2569de46ec
Aftab-Home <aftab.pilover@gmail.com>
2025-10-15 20:49:24 +0600

    CheckPoints defined

---
+++ Assets/MainProject/Codes/CheckPoints.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aftab
+{
+    public class CheckPoints : MonoBehaviour
+    {
+        [Header("Settings:")]
+        [SerializeField]
+        List<Transform> checkPointsTr = new List<Transform>();
+        int currentCheckPointIndex = 0;
+
+        public Transform GetCurrentCheckPoint()
+        {
+            if (checkPointsTr == null) return null;
+            if (checkPointsTr.Count <= 0) return null;
+            return checkPointsTr[currentCheckPointIndex];
+        }
+
+        public void SetCurrentCheckPointIndexOnArrival()
+        {
+            currentCheckPointIndex++;
+            if (currentCheckPointIndex >= checkPointsTr.Count) currentCheckPointIndex = 0;
+        }
+
+        public Transform GetNextCheckPoint()
+        {
+            if (checkPointsTr == null) return null;
+            if (checkPointsTr.Count <= 0) return null;
+            int nextCheckPointIndex = currentCheckPointIndex + 1;
+            if (nextCheckPointIndex >= checkPointsTr.Count) nextCheckPointIndex = 0;
+            return checkPointsTr[nextCheckPointIndex];
+        }
+    }
+}
