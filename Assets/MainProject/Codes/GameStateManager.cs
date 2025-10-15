efd294dd4f586dad2f126851414d14084fb30cff
Aftab-Home <aftab.pilover@gmail.com>
2025-10-15 21:44:58 +0600

    GameStateManager with GameStates enum and GameManager added

---
+++ Assets/MainProject/Codes/GameStateManager.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+namespace Aftab
+{
+    public class GameStateManager : MonoBehaviour
+    {
+        public static GameStateManager Instance { get; private set; }
+        private GameStates currentGameStates = GameStates.Initial;
+        public event Action<GameStates> OnGameStateChanged;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public void SetGameState(GameStates state)
+        {
+            if (currentGameStates == state) return;
+            currentGameStates = state;
+            OnGameStateChanged?.Invoke(state);
+        }
+    }
+
+    public enum GameStates
+    {
+        Initial = 0,
+        Playing = 1,
+        Paused = 2
+    }
+}
