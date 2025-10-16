f6dda21c14bde73839ebfa5f6c556094611ea2b9
Aftab-Home <aftab.pilover@gmail.com>
2025-10-16 11:12:36 +0600

    TheMan movement cont.

--- Assets/MainProject/Codes/TheManMovement.cs
+++ Assets/MainProject/Codes/TheManMovement.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Aftab
@@ -8,23 +7,62 @@ namespace Aftab
     {
         [Header("Settings:")]
         [SerializeField]
-        Transform transformToMove;
+        private Transform transformToMove;
         [SerializeField]
-        float movementSpeed = 5.0f;
+        private float movementSpeed = 5.0f;
+        private Coroutine currentCR = null;
 
-        bool canMove = false;
-
-        GameStateManager gameStateManager;
+        private GameStateManager gameStateManager;
 
         private void Start()
         {
             gameStateManager = GameStateManager.Instance;
             gameStateManager.OnGameStateChanged += OnGameStateChange;
+            transformToMove.position = CheckPoints.Instance.GetCurrentCheckPoint().position;
         }
 
         private void OnDisable()
         {
             gameStateManager.OnGameStateChanged -= OnGameStateChange;
+        }
+
+        private void ManageMovement(bool move)
+        {
+            CleanCurrentCR();
+            if(move) currentCR = StartCoroutine(MovementCR());
+
+            IEnumerator MovementCR()
+            {
+                yield return null;
+
+                Transform currenCheckPoint = CheckPoints.Instance.GetCurrentCheckPoint();
+                Transform nextCheckPoint = CheckPoints.Instance.GetNextCheckPoint();
+                Vector3 lookDirection = nextCheckPoint.position - currenCheckPoint.position;
+                lookDirection.y = 0;
+                if (lookDirection != Vector3.zero)
+                {
+                    Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+                    transformToMove.rotation = targetRotation;
+                }
+
+                while (Vector3.Distance(transformToMove.position, nextCheckPoint.position) > 0.1f)
+                {
+                    transformToMove.position = Vector3.MoveTowards(
+                        transformToMove.position, nextCheckPoint.position, movementSpeed * Time.deltaTime);
+                    yield return null;
+                }
+                transformToMove.position = nextCheckPoint.position;
+                CheckPoints.Instance.SetCurrentCheckPointIndexOnArrival();
+                yield return null;
+                ManageMovement(move); //Recursively calling the method again
+            }
+
+            void CleanCurrentCR()
+            {
+                if (currentCR == null) return;
+                StopCoroutine(currentCR);
+                currentCR = null;
+            }
         }
 
         private void OnGameStateChange(GameStates gameState)
@@ -33,19 +71,15 @@ namespace Aftab
             {
                 case GameStates.Initial:
                     //Stand idle
-                    canMove = false;
                     break;
                 case GameStates.Playing:
                     //Run
-                    canMove = true;
                     break;
                 case GameStates.Paused:
                     //Stand idle
-                    canMove = false;
                     break;
                 default:
                     //Stand idle
-                    canMove = false;
                     break;
             }
         }
